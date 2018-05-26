1398c24e100827dec132f1e5b36c0626928ea576
Leslie <leslie.ino@gmail.com>
2018-05-26 13:08:57 -0400

    segundo cambio de input a button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* Acá va tu código */
 window.onload = function(){
     let text = document.getElementById("button1").value;
-    let textdos = document.getElementById("descifrar").value;
+    let textdos = document.getElementById("button2").value;
 
 
 } 
