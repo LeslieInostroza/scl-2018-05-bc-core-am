d721afa888b42d1c5bdb6a20ad6f01ba6f1b61c0
Leslie <leslie.ino@gmail.com>
2018-05-30 18:22:38 -0400

    por fin el DOM

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ window.onload = function(){
     // ---> ()=> es una funcion <----
     button1.addEventListener('click', () =>{
      let cifrar = document.getElementById('input_cifrar').value;
-          texto.innerHTML = "su resultado es:" + input_cifrar;
+          texto.innerHTML = "su resultado es: " + cifrar;
     });
     button2.addEventListener('click', () =>{
       let descifrar = document.getElementById('input_descifrar').value;
-        texto2.innerHTML = "su resultado es:" + input_descifrar;
+        texto2.innerHTML = "su resultado es: " + descifrar;
     });
 
 } 
