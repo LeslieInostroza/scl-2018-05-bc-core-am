fb21e6be8c03b3333dfac8d705b0b0778a86756c
Leslie <leslie.ino@gmail.com>
2018-05-30 18:00:02 -0400

    arreglo js DOM

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@
 window.onload = function(){
     const button1 = document.getElementById("button1");
     const button2 = document.getElementById("button2");
+    const caracter = document.getElementById("caracter");
+    let texto = document.getElementById("texto");
+    let texto2 = document.getElementById("texto2");
 
     // ---> ()=> es una funcion <----
     button1.addEventListener('click', () =>{
-     let cifrar = document.getElementById('cajacifrar').value;
-         cajadescifrar.value = cifrar;
+     let cifrar = document.getElementById('input_cifrar').value;
+          texto.innerHTML = "su resultado es:" + input_cifrar;
     });
     button2.addEventListener('click', () =>{
-      let descifrar = document.getElementById('cajadescifrar').value;
-         cajacifrar.value = descifrar;
+      let descifrar = document.getElementById('input_descifrar').value;
+        texto2.innerHTML = "su resultado es:" + input_descifrar;
     });
 
 } 
