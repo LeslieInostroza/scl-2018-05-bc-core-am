9aeb375285f8e737e0d21c6a675fe554c93390dc
Leslie <leslie.ino@gmail.com>
2018-05-29 20:00:01 -0400

    ultimos detalles css, HTML

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 /* Acá va tu código */
 window.onload = function(){
-    let button1 = document.getElementById("button1");
-    let button2 = document.getElementById("button2");
+    const button1 = document.getElementById("button1");
+    const button2 = document.getElementById("button2");
 
-    button1.addEventListener('click', function(){
+    button1.addEventListener('click', () =>{
      let cifrar = document.getElementById('cajacifrar').value;
           console.log(cifrar);
     });
 
-    button2.addEventListener('click', function(){
+    button2.addEventListener('click', () =>{
       let descifrar = document.getElementById('cajadescifrar').value;
           console.log(descifrar);
     });
