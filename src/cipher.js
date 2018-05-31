baa07e2b619c816d08a9a6cc6ddc4d01c520de65
Leslie <leslie.ino@gmail.com>
2018-05-31 00:26:46 -0400

    avance funcion

--- src/cipher.js
+++ src/cipher.js
@@ -1,13 +1,20 @@
 window.cipher = {
   encode: (cart,cifrar) => {
     /* Acá va tu código */
-    let msj ="";
+    let result ="";
     for(let i = 0; i< cifrar.lenght; i++ );
+    let inc = cifrar.charCodeAt(i);
+    let ecuacion = ((inc-65 + cart)% 26 + 65);
+    result += String.fromCharCode(ecuacion);
+
     
   },
   decode: (cartDos,descifrar) => {
     /* Acá va tu código */
-    let msjdos="";
+    let resultdos="";
     for(let i = 0; i< descifrar.lenght; i++);
+    let incg = descifrar.charCodeAt(i);
+    let ecuaDos = ((incg + 65 - cartDos)% 26 -65);
+    result += String.fromCharCode(ecuaDos);
   }
 }
