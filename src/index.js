82e1c02a9b044f40de686cb8cfcf101388c71e49
Leslie <leslie.ino@gmail.com>
2018-05-31 10:33:03 -0400

    ultimos arreglos

--- src/index.js
+++ src/index.js
@@ -9,13 +9,10 @@ window.onload = function(){
   button1.addEventListener('click', () =>{
   let cifrar = document.getElementById('input_cifrar').value.toUpperCase();
   console.log(cifrar);
-  
   let cart = document.getElementById("caracter").value;
   console.log(cart);
-  
   let ciph = window.cipher.encode(cifrar, cart);
   console.log(ciph);
-  
   texto.innerHTML = `su resultado cifrado es: ${ciph}`;
   });
 
@@ -23,13 +20,10 @@ window.onload = function(){
   button2.addEventListener('click', () =>{
   let descifrar=document.getElementById('input_descifrar').value.toUpperCase();
   console.log(descifrar);
-
   let cartDos = document.getElementById("caracterDos").value;
   console.log(cartDos);
-
   let diceph = window.cipher.decode(descifrar, cartDos);
   console.log(diceph);
-
   texto2.innerHTML = `su resultado descifrado es: ${diceph}`;
   
     });
