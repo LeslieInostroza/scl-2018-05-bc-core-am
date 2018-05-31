9d541ace9bfb4075c3cf992db3af406efa642c23
Leslie <leslie.ino@gmail.com>
2018-05-30 23:26:25 -0400

    empezando la funcion cifrado, arreglos en js y html

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,9 @@
 window.cipher = {
-  encode: () => {
+  encode: (crt,cifrar) => {
     /* Acá va tu código */
+    let mensaje ="";
+    for(let i = 0; i< cifrar.lenght; i++ );
+    
   },
   decode: () => {
     /* Acá va tu código */
--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 /* Acá va tu código */
 window.onload = function(){
-    const button1 = document.getElementById("button1");
-    const button2 = document.getElementById("button2");
-    const caracter = document.getElementById("caracter");
-    let texto = document.getElementById("texto");
-    let texto2 = document.getElementById("texto2");
+  const button1 = document.getElementById("button1");
+  const button2 = document.getElementById("button2");
+  let texto = document.getElementById("texto");
+  let texto2 = document.getElementById("texto2");
 
     // ---> ()=> es una funcion <----
-    button1.addEventListener('click', () =>{
-     let cifrar = document.getElementById('input_cifrar').value;
-          texto.innerHTML = "su resultado es: " + cifrar;
+  button1.addEventListener('click', () =>{
+     let cifrar = document.getElementById('input_cifrar').value.toUpperCase();
+     let crt = document.getElementById("caracter");
+     texto.innerHTML = "su resultado descifrado es: " + cifrar;
+     let cipher = window.cipher.encode(caracter, cifrar);
     });
-    button2.addEventListener('click', () =>{
-      let descifrar = document.getElementById('input_descifrar').value;
-        texto2.innerHTML = "su resultado es: " + descifrar;
+  button2.addEventListener('click', () =>{
+   let descifrar=document.getElementById('input_descifrar').value.toUpperCase();
+   let crtDos = document.getElementById("caracterDos");
+     texto2.innerHTML = "su resultado cifrado es: " + descifrar;
     });
 
 } 
