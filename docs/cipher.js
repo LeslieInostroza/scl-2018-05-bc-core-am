5abf64d0b84f028c27dc9ec3f2dbb8818cd66a72
Leslie <leslie.ino@gmail.com>
2018-06-01 09:52:31 -0400

    añadi carpta docs

---
+++ docs/cipher.js
@@ -0,0 +1,24 @@
+window.cipher = {
+  encode: (cifrar, cart) => {
+    /* Acá va tu código */
+    let result =""; // acá se concadenan mi texto ya cifrado
+    for(let i = 0; i< cifrar.length; i++ ) { // ciclo for, recorre la palabra escrita por el usuario
+     //let inc = cifrar.charCodeAt(i); 
+     //calcula cuantos espacios se movera,segun indica el usuario, tranforma string a codigo ascii,(a n°) ej:A=65
+     let ecuacion = ((cifrar.charCodeAt(i) - 65 + parseInt(cart)) % 26 + 65);
+     result += String.fromCharCode(ecuacion);//transforma el n° de la ecuacion en string
+     //console.log(result);
+    }
+    return result;
+  },
+  decode: (descifrar, cartDos) => {
+    /* Acá va tu código */
+    let resultdos=""; // acá se concadenan mi texto ya descifrado
+    for(let i = 0; i< descifrar.length; i++) {
+     //let incg = descifrar.charCodeAt(i);
+     let ecuaDos = ((descifrar.charCodeAt(i) + 65 - parseInt(cartDos)) % 26 + 65);
+     resultdos += String.fromCharCode(ecuaDos);
+  }
+  return resultdos;
+  }
+}
---
+++ docs/index.js
@@ -0,0 +1,31 @@
+/* Acá va tu código */
+window.onload = () => {
+  const button1 = document.getElementById("button1");
+  const button2 = document.getElementById("button2");
+  let texto = document.getElementById("texto");
+  let texto2 = document.getElementById("texto2");
+
+  // ---> ()=> es una funcion <----
+  button1.addEventListener('click', () => {
+   let cifrar = document.getElementById("input_cifrar").value.toUpperCase();
+   //console.log(cifrar);
+   let cart = document.getElementById("caracter").value;
+   //console.log(cart);
+   let ciph = window.cipher.encode(cifrar, cart);
+   //console.log(ciph);
+   texto.innerHTML = `su resultado cifrado es: ${ciph}`;
+  });
+
+ // ---> mi descifrado <-----
+  button2.addEventListener('click', () => {
+   let descifrar=document.getElementById('input_descifrar').value.toUpperCase();
+   //console.log(descifrar);
+   let cartDos = document.getElementById("caracterDos").value;
+   //console.log(cartDos);
+   let diceph = window.cipher.decode(descifrar, cartDos);
+   //console.log(diceph);
+   texto2.innerHTML = `su resultado descifrado es: ${diceph}`;
+  
+  });
+
+} 
